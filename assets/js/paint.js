685967d8e628178e234de5b1f6edc2c00ad3a835
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2022-08-31 22:58:36 -0700

    auto formatted paint.js, added event listener in for loop to add mouseover to each img

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -2,71 +2,70 @@ function configureListeners() {
     // select img elements  
     let images = document.querySelectorAll('img')
 
-
-     for (var i = 0; i < images.length; i++) {        
-        // iterate over images and add mouseover event listeners      
-    } 
+    for (var i = 0; i < images.length; i++) {
+        // iterate over images and add mouseover event listeners
+        images[i].addEventListener('mouseover', getProductInfo(this.id))
+    }
 }
 
 function addOpacity(event) {
     // add appropriate CSS class
-    getProductInfo(event.target.id);     
+    getProductInfo(event.target.id);
 }
 
 function removeOpacity(event) {
-     //remove appropriate CSS class
+    //remove appropriate CSS class
 
     let element = document.getElementById('color-price');
-        element.textContent = '';
-        
-    let color = document.getElementById('color-name');
-        color.textContent = ''; 
+    element.textContent = '';
 
-    event.preventDefault();    
+    let color = document.getElementById('color-name');
+    color.textContent = '';
+
+    event.preventDefault();
 }
 
 function getProductInfo(paintColor) {
     let price;
-    let colorName;  
-    
+    let colorName;
+
     switch (paintColor) {
-        case 'pn1':           
+        case 'pn1':
             // set variables for price and color name and invoke a function to update the price     
-            break;           
+            break;
         case 'pn2':
             // set variables for price and color name and invoke a function to update the price    
-            break;            
+            break;
         case 'pn3':
             // set variables for price and color name and invoke a function to update the price  
-            break;   
+            break;
         case 'pn4':
             // set variables for price and color name and invoke a function to update the price  
-            break;   
+            break;
         case 'pn5':
             // set variables for price and color name and invoke a function to update the price       
-            break;   
+            break;
         case 'pn6':
             // set variables for price and color name and invoke a function to update the price        
-            break;   
+            break;
         case 'pn7':
             // set variables for price and color name and invoke a function to update the price 
-            break;   
+            break;
         case 'pn8':
             // set variables for price and color name and invoke a function to update the price   
-            break;   
+            break;
         case 'pn9':
             // set variables for price and color name and invoke a function to update the price 
-            break;   
-          default:              
+            break;
+        default:
     }
 
-    function updatePrice(colorName, price)
-    {       
-        let colorPrice = // select element with corresponding id
-        // display price
-        
-        let color = // select element with corresponding id
+    function updatePrice(colorName, price) {
+        let colorPrice //= // select element with corresponding id
+            // display price
+
+        let color //= // select element with corresponding id
         //display color name
     }
-    
+
 }
